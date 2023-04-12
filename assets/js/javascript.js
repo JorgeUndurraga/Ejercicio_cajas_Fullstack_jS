d6891e86ab504ef098f630a4ff53ae3b444cd820
JorgeUndurraga <jorgeundurraga@yahoo.com>
2023-04-12 03:48:09 -0400

    subiendo proyecto a Git

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+let elementId = document.getElementById("info2")
+console.log(elementId);
+
+let elementosClase = document.getElementsByClassName("container-fluid")
+console.log(elementosClase[0])
+// console.log(elementosClase);
+
+var elementosTag = document.getElementsByTagName("article");
+console.log(elementosTag[0])
+// console.log(elementosTag);
